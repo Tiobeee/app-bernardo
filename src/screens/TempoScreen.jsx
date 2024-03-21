4c3840bdb79fa198e0f92c65de2c99375e8774d1
Tiobee <bernardo.budal10@gmail.com>
2024-03-21 16:22:14 -0300

    Tempo Screen funcionando

--- src/screens/TempoScreen.jsx
+++ src/screens/TempoScreen.jsx
@@ -1,42 +1,55 @@
 import { View } from "react-native"; // importei do native
-import { Text } from "react-native-paper"; // importei do native paper
+import { Card, Text } from "react-native-paper"; // importei do native paper
 import styles from "../config/styles";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const API_KEY = "e92dc176abd6c90271e75b1a10b4df83";
-const CITY_NAME = "Joinville, SC";//peguem a de vocês
+const CITY_NAME = "Joinville";//peguem a de vocês
 
 
-export default function TempoScreen(){
-    
-    useEffect(()=>{
-        const fetchTempo = async () =>{
-            //recebe o valor da api em texto
-                const url = `https://api.openweathermap.org/data/2.5/weather?q=${CITY_NAME}&appid=${API_KEY}&units=metric`;
-        // recebe o valor do api em texto
-        const resposta = fetch(url, {
-            // fix mime type
-            headers: {
-            "Content-Type": "application/json",
-            Accept: "application/json",
-            },
-        });
-
-            //basico de fetch json
-            //transforma o texto em json
-            const data = await resposta.json();
-            console.log(data);
+export default function TempoScreen() {
+    const [tempoData, setTempoData] = useState(null);
+  
+    useEffect(() => {
+      const fetchTempo = async () => {
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${CITY_NAME}&appid=${API_KEY}&units=metric`;
+  
+        // Faça a requisição usando Axios
+        try {
+          const response = await axios.get(url);
+          console.log(response.data);
+          // // A resposta está disponível no objeto response.data
+  
+          setTempoData(response.data);
+        } catch (error) {
+          // Trate o erro
+          console.error("There was an error!", error);
         }
-        fetchTempo();
-    }, []);
-
-    return(
-        <View
-            style={styles.container}
-        >
-            <Text
-                variant="bodyLarge" //variantes que só o papel possui
-            >Tempo</Text>
-        </View>
-    )
-}
+      };
+      fetchTempo();
+    }, []); // array vazio no final de useEffect simula simboliza que a função será executada apenas uma vez, quando o componente for montado
+  
+    return (
+      <View style={styles.container}>
+        <Text variant="bodyLarge">Tempo em {CITY_NAME}</Text>
+        {tempoData && (
+          <Card style={styles.card}>
+            <Card.Title title="Detalhes do Tempo" />
+            <Card.Content>
+              <Text>Temperatura atual cai de boca: {tempoData.main.temp}°C</Text>
+              {/* <Text>Sensação térmica: {tempoData.main.feels_like}°C</Text>
+              <Text>Temperatura mínima: {tempoData.main.temp_min}°C</Text>
+              <Text>Temperatura máxima: {tempoData.main.temp_max}°C</Text>
+              <Text>Pressão: {tempoData.main.pressure} hPa</Text>
+              <Text>Umidade: {tempoData.main.humidity}%</Text>
+              <Text>Visibilidade: {tempoData.visibility / 1000} km</Text>
+              <Text>Vento: {tempoData.wind.speed} m/s</Text>
+              <Text>Descrição: {tempoData.weather[0].description}</Text> */}
+            </Card.Content>
+          </Card>
+        )}
+      </View>
+    );
+  }
+  
